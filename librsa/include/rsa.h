f2dd0cbc7d81470ce5e7c122567bf00cb9dd55f9
Juan Pablo Brenes C <juanp1995@gmail.com>
2020-05-17 02:07:45 -0600

    Function to load a key file

--- librsa/include/rsa.h
+++ librsa/include/rsa.h
@@ -8,12 +8,14 @@
 #include <openssl/bio.h>
 #include <openssl/err.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 RSA * createRSA(unsigned char * key,int public);
 int public_encrypt(unsigned char * data,int data_len,unsigned char * key, unsigned char *encrypted);
 int private_decrypt(unsigned char * enc_data,int data_len,unsigned char * key, unsigned char *decrypted);
 int private_encrypt(unsigned char * data,int data_len,unsigned char * key, unsigned char *encrypted);
 int public_decrypt(unsigned char * enc_data,int data_len,unsigned char * key, unsigned char *decrypted);
+unsigned char * loadKey(char *pFile);
 
 
 
--- librsa/lib/rsa.c
+++ librsa/lib/rsa.c
@@ -2,6 +2,34 @@
 
 int padding = RSA_PKCS1_PADDING;
 
+// char privKey[] = "-----BEGIN RSA PRIVATE KEY-----\n"\
+// "MIIEpgIBAAKCAQEA1JmCh5ltimuTMBrw7Zvt0aHZUTCwy8av3Ceo9BmFVbBthA/I\n"\
+// "rFGs9LRChb441ucoe7JRC2P6xB3TDtW23ZyVuBK7qAHV2WCA0LpAXWd4EPSiae10\n"\
+// "8jJiH/INUHPdn6UguYJWD0ywctLcEOs2gVNQsxcqdLlhCsMO5pjkTlhhJa9bkFzm\n"\
+// "CyaybJ3yXidTyteAn4wDADfhWoi9vjbo4cMkrbxQ95cbHpuwjVQjx3nq5w6wvt6x\n"\
+// "RyHuzRWFzDvCmkG7R+XD+Fd95nMu09E29wQrulbzNCHeDioqZJscJrCyIWtaWNQW\n"\
+// "olycb6BkaVgkN8XsM1vcibS5efdiL5drtzyBEwIDAQABAoIBAQDCJrbHstwQjxFJ\n"\
+// "2zCvLblPumjF9pteXUJ44VcwJKZZjNdWxTtT8vrqNjyR+xfZWhzkagINTu7RbxMx\n"\
+// "9wYa+EbaZXEuiw6Kh9DbzVu+cSF7Co3QAuTL0is+VUTHdiitBJYQE/EX48c39Uy3\n"\
+// "vrZ2AGowQzWohysSMvRGrtXAnoGJcSMnZ8Pn5f/sL+YUK3HqgXChcWXBytMjxPSj\n"\
+// "AKfYmyVB8lC3tWspmD/W4FFtU1rSaziSWogei11NjkVd7bzy+JJyCxNPgaGbd88S\n"\
+// "Ej13avTINFsapDvhJl9NW76CvIQifmmvHhRTQ9dRBDA9tzc1WBKlfYZUpbk5O1Dh\n"\
+// "Omm03i2xAoGBAOqGETGAH6lSUqLnMUDoh4DsCNdfAH4gzXzi3GIsM7v6VaMqhBGl\n"\
+// "0LJ4ohV/SQrd448YIRyaQ1gJnL1btzVoTYZp38qb/EPsKAByNN2vyg0qclCh9gK/\n"\
+// "JB6S9EHXdWbdBqUygf6lvWTkVco5Lc6HoujtzEqCTbQEhVmSHL+mQOLVAoGBAOgR\n"\
+// "ek5F2Iqab6QjlsYY2gHcniE43QdWM38ra5bYuXKNGEoG+w3srX1cmOdTSHW6LrzA\n"\
+// "DswYwVl7bzBuCm3dXe2hJmYzMhCgx5Kz+FBfzYzyfWzgljK7ZMvbQRYdDcVHYGvC\n"\
+// "wNhx9TB9yR8I6PszzjfrWb6xP2NKmeGNTiQMozhHAoGBANSNP6Xybj+iqdyU1pHy\n"\
+// "bWUOB5FSu/qUNkj6vyyGzTiQUO85yErKUpR+OsBrBUNBCU4WW2Q199UhmRoRtsFD\n"\
+// "0AeDi11w9DiHH1YQLBPDj8dQ8hxcPcANhmnjXFcUNIZOyggM975YboezXoGSQw7M\n"\
+// "5XxgkWfyIwSFmGouv7l+r44lAoGBALmdERf94X6qm1TLalKzmdsXXBryYkb1+WZj\n"\
+// "kkAqw9kNZToAb1jpzrGQGHQdNr2C1tL9QliYDD3H7lzsBsds4S2pUQ85L3gBQN7j\n"\
+// "t16B4eQ0J4Gn+DpHjA1JJrvLLTJ3LnXCZYA5VYUddnmuJLqW6v/q6/MLzcla581x\n"\
+// "ueg3e20NAoGBAMas0vhaFI1Sd87J1gVAWpcWIgkMdG+3kIMqDHgKKc+NvSGKD6lh\n"\
+// "eYVTAiBZquQXyWG2bnLg27WouGb61IZ/0bO0hyOxyWO+0nmnHpzWB5RJcTAem3JJ\n"\
+// "lGzrdcmjbo7AWn3bgqsl6k1ZbfRxCLBEhnQuK8f6WGkz3G5soVNBc0Zv\n"\
+// "-----END RSA PRIVATE KEY-----\n";
+
 RSA * createRSA(unsigned char * key,int public) {
     RSA *rsa= NULL;
     BIO *keybio ;
@@ -24,116 +52,101 @@ RSA * createRSA(unsigned char * key,int public) {
     return rsa;
 }
 
+/**
+ * Encrypt data with public key
+ *
+ * @param  data      Data to encrypt
+ * @param  data_len  Length of data
+ * @param  key       Public key
+ * @param  encrypted Encryption output
+ * @return           0 if successful, -1 in case of error
+ */
 int public_encrypt(unsigned char * data,int data_len,unsigned char * key, unsigned char *encrypted) {
     RSA * rsa = createRSA(key,1);
     int result = RSA_public_encrypt(data_len,data,encrypted,rsa,padding);
     return result;
 }
 
+/**
+ * Decrypt data with the private key
+ *
+ * @param  enc_data  Encrypted data
+ * @param  data_len  Length of encrypted data (256bits for 2048bits key)
+ * @param  key       Private key
+ * @param  decrypted Decrypted data
+ * @return           0 if successful, -1 in case of error
+ */
 int private_decrypt(unsigned char * enc_data,int data_len,unsigned char * key, unsigned char *decrypted) {
     RSA * rsa = createRSA(key,0);
     int  result = RSA_private_decrypt(data_len,enc_data,decrypted,rsa,padding);
     return result;
 }
 
-
+/**
+ * Encrypt data with the private key
+ *
+ * @param  data      Data to encrypt
+ * @param  data_len  Length of data
+ * @param  key       Private key
+ * @param  encrypted Encrypted data
+ * @return           0 if successful, -1 in case of error
+ */
 int private_encrypt(unsigned char * data,int data_len,unsigned char * key, unsigned char *encrypted) {
     RSA * rsa = createRSA(key,0);
     int result = RSA_private_encrypt(data_len,data,encrypted,rsa,padding);
     return result;
 }
 
+/**
+ * Decrypt data with public key
+ *
+ * @param  enc_data  Encrypted data
+ * @param  data_len  Length of encrypted data
+ * @param  key       Public key
+ * @param  decrypted Decrypted data
+ * @return           0 if successful, -1 in case of error
+ */
 int public_decrypt(unsigned char * enc_data,int data_len,unsigned char * key, unsigned char *decrypted) {
 
     RSA * rsa = createRSA(key,1);
     int  result = RSA_public_decrypt(data_len,enc_data,decrypted,rsa,padding);
-    return result;    
+    return result;
 }
 
+/**
+ * Loads a key from a file to a char array
+ * 
+ * @param  pFile File path
+ * @return       Char array with key
+ */
+unsigned char * loadKey(char *pFile){
+    FILE *keyFD;
+    long size;
+    unsigned char *keyBuffer;
 
-// int main(){
-//
-//     char plainText[2048/8] = "Hello this is Ravi"; //key length : 2048
-//
-//
-//     char publicKey[]="-----BEGIN PUBLIC KEY-----\n"\
-//     "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy8Dbv8prpJ/0kKhlGeJY\n"\
-//     "ozo2t60EG8L0561g13R29LvMR5hyvGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+\n"\
-//     "vw1HocOAZtWK0z3r26uA8kQYOKX9Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQAp\n"\
-//     "fc9jB9nTzphOgM4JiEYvlV8FLhg9yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68\n"\
-//     "i6T4nNq7NWC+UNVjQHxNQMQMzU6lWCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoV\n"\
-//     "PpY72+eVthKzpMeyHkBn7ciumk5qgLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUy\n"\
-//     "wQIDAQAB\n"\
-//     "-----END PUBLIC KEY-----\n";
-//
-//     char privateKey[]="-----BEGIN RSA PRIVATE KEY-----\n"\
-//     "MIIEowIBAAKCAQEAy8Dbv8prpJ/0kKhlGeJYozo2t60EG8L0561g13R29LvMR5hy\n"\
-//     "vGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+vw1HocOAZtWK0z3r26uA8kQYOKX9\n"\
-//     "Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQApfc9jB9nTzphOgM4JiEYvlV8FLhg9\n"\
-//     "yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68i6T4nNq7NWC+UNVjQHxNQMQMzU6l\n"\
-//     "WCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoVPpY72+eVthKzpMeyHkBn7ciumk5q\n"\
-//     "gLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUywQIDAQABAoIBADhg1u1Mv1hAAlX8\n"\
-//     "omz1Gn2f4AAW2aos2cM5UDCNw1SYmj+9SRIkaxjRsE/C4o9sw1oxrg1/z6kajV0e\n"\
-//     "N/t008FdlVKHXAIYWF93JMoVvIpMmT8jft6AN/y3NMpivgt2inmmEJZYNioFJKZG\n"\
-//     "X+/vKYvsVISZm2fw8NfnKvAQK55yu+GRWBZGOeS9K+LbYvOwcrjKhHz66m4bedKd\n"\
-//     "gVAix6NE5iwmjNXktSQlJMCjbtdNXg/xo1/G4kG2p/MO1HLcKfe1N5FgBiXj3Qjl\n"\
-//     "vgvjJZkh1as2KTgaPOBqZaP03738VnYg23ISyvfT/teArVGtxrmFP7939EvJFKpF\n"\
-//     "1wTxuDkCgYEA7t0DR37zt+dEJy+5vm7zSmN97VenwQJFWMiulkHGa0yU3lLasxxu\n"\
-//     "m0oUtndIjenIvSx6t3Y+agK2F3EPbb0AZ5wZ1p1IXs4vktgeQwSSBdqcM8LZFDvZ\n"\
-//     "uPboQnJoRdIkd62XnP5ekIEIBAfOp8v2wFpSfE7nNH2u4CpAXNSF9HsCgYEA2l8D\n"\
-//     "JrDE5m9Kkn+J4l+AdGfeBL1igPF3DnuPoV67BpgiaAgI4h25UJzXiDKKoa706S0D\n"\
-//     "4XB74zOLX11MaGPMIdhlG+SgeQfNoC5lE4ZWXNyESJH1SVgRGT9nBC2vtL6bxCVV\n"\
-//     "WBkTeC5D6c/QXcai6yw6OYyNNdp0uznKURe1xvMCgYBVYYcEjWqMuAvyferFGV+5\n"\
-//     "nWqr5gM+yJMFM2bEqupD/HHSLoeiMm2O8KIKvwSeRYzNohKTdZ7FwgZYxr8fGMoG\n"\
-//     "PxQ1VK9DxCvZL4tRpVaU5Rmknud9hg9DQG6xIbgIDR+f79sb8QjYWmcFGc1SyWOA\n"\
-//     "SkjlykZ2yt4xnqi3BfiD9QKBgGqLgRYXmXp1QoVIBRaWUi55nzHg1XbkWZqPXvz1\n"\
-//     "I3uMLv1jLjJlHk3euKqTPmC05HoApKwSHeA0/gOBmg404xyAYJTDcCidTg6hlF96\n"\
-//     "ZBja3xApZuxqM62F6dV4FQqzFX0WWhWp5n301N33r0qR6FumMKJzmVJ1TA8tmzEF\n"\
-//     "yINRAoGBAJqioYs8rK6eXzA8ywYLjqTLu/yQSLBn/4ta36K8DyCoLNlNxSuox+A5\n"\
-//     "w6z2vEfRVQDq4Hm4vBzjdi3QfYLNkTiTqLcvgWZ+eX44ogXtdTDO7c+GeMKWz4XX\n"\
-//     "uJSUVL5+CVjKLjZEJ6Qc2WZLl94xSwL71E41H4YciVnSCQxVc4Jw\n"\
-//     "-----END RSA PRIVATE KEY-----\n";
-//
-//
-//     unsigned char  encrypted[4098]={};
-//     unsigned char decrypted[4098]={};
-//
-//     int encrypted_length= public_encrypt(plainText,strlen(plainText),publicKey,encrypted);
-//     if(encrypted_length == -1)
-//     {
-//         printLastError("Public Encrypt failed ");
-//         exit(0);
-//     }
-//     printf("Encrypted length =%d\n",encrypted_length);
-//
-//     int decrypted_length = private_decrypt(encrypted,encrypted_length,privateKey, decrypted);
-//     if(decrypted_length == -1)
-//     {
-//         printLastError("Private Decrypt failed ");
-//         exit(0);
-//     }
-//     printf("Decrypted Text =%s\n",decrypted);
-//     printf("Decrypted Length =%d\n",decrypted_length);
-//
-//
-//     encrypted_length= private_encrypt(plainText,strlen(plainText),privateKey,encrypted);
-//     if(encrypted_length == -1)
-//     {
-//         printLastError("Private Encrypt failed");
-//         exit(0);
-//     }
-//     printf("Encrypted length =%d\n",encrypted_length);
-//
-//     decrypted_length = public_decrypt(encrypted,encrypted_length,publicKey, decrypted);
-//     if(decrypted_length == -1)
-//     {
-//         printLastError("Public Decrypt failed");
-//         exit(0);
-//     }
-//     printf("Decrypted Text =%s\n",decrypted);
-//     printf("Decrypted Length =%d\n",decrypted_length);
-//
-//
-//
-//
-// }
+    keyFD = fopen(pFile, "r");
+    if(keyFD == NULL){
+        return "1";
+    }
+
+    fseek(keyFD, 0L, SEEK_END);
+    size = ftell(keyFD);
+    rewind(keyFD);
+
+    //privateKey = calloc(1, size+1);
+    keyBuffer = (unsigned char *)malloc(size);
+    if(!keyBuffer){
+        fclose(keyFD);
+        return "2";
+    }
+
+    //fread(buffer, fileLen, sizeof(unsigned char), file);
+    if( 1 != fread(keyBuffer, size, sizeof(unsigned char), keyFD)){
+        fclose(keyFD);
+        free(keyBuffer);
+        return "3";
+    }
+
+    fclose(keyFD);
+    return keyBuffer;
+}
